7b576d06ccceb7c33a870ce25d57f88fbcee98d2
Guillaume Wuip <guillaume@wuips.com>
2017-03-11 10:56:30 +0100

    Add Pixi.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,17 @@
 
+import * as PIXI from 'pixi.js';
+
 require('../sass/styles.scss');
 
 console.log(`🤖 Parrains2017 v${VERSION}`);// eslint-disable-line no-undef
+
+const renderer = PIXI.autoDetectRenderer(256, 256);
+const canvas = document.querySelector('.js-canvas-container');
+
+console.log(canvas);
+
+canvas.appendChild(renderer.view);
+
+const stage = new PIXI.Container();
+
+renderer.render(stage);
