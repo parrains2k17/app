0216c0f3cf91d7d26c1cae1e5dd0ca4f95d6021b
Guillaume Wuip <guillaume@wuips.com>
2017-03-12 07:38:20 +0100

    Supporters rotating around Candidates

--- js/components/Circle.js
+++ js/components/Circle.js
@@ -1,35 +1,31 @@
 
 import { Graphics } from 'pixi.js';
-import { TweenMax, Power0 } from 'gsap';
 
-// circle that rotate around a point
+import { WHITE } from '../style/color';
+
 class Circle extends Graphics {
-    constructor(x = 0, y = 0, radius = 1, alpha = 1, delay = 0) {
+    constructor({
+        position = { x: 0, y: 0 },
+        offset = { x: 0, y: 0 },
+        rotation = 0,
+        alpha = 1,
+        radius = 1,
+        color = WHITE,
+        pivot = { x: 0, y: 0 },
+    }) {
         super();
-        this.beginFill(0x9966FF);
-        this.drawCircle(0, 0, radius);
+        this.beginFill(color);
+        this.drawCircle(offset.x, offset.y, radius);
         this.endFill();
 
         this.alpha = alpha;
+        this.position = position;
+        this.pivot = pivot;
+        this.rotation = rotation;
 
-        this.x = x;
-        this.y = y;
-
-        this.pivot.set(20, 20);
-
-        TweenMax.fromTo(
-            this,
-            3,
-            {
-                rotation: 0,
-            },
-            {
-                delay,
-                rotation: Math.PI * 2,
-                repeat:   -1,
-                ease:     Power0.easeNone,
-            }
-        );
+        // non-pixi attributes
+        this.startPosition = position;
+        this.offset = offset;
     }
 }
 
---
+++ js/style/color.js
@@ -0,0 +1,4 @@
+
+export const PURPLE = 0x9966FF;
+export const BLACK  = 0x000000;
+export const WHITE  = 0xFFFFFF;
