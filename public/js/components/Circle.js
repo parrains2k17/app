9839c7bf28478932630ef618c0f8f1d725bb05d4
Thomas Coquereau <thomas.coquereau@etu.univ-nantes.fr>
2017-03-12 10:56:39 +0100

    ADD new colors and random selection of a supporter's color

--- public/js/components/Circle.js
+++ public/js/components/Circle.js
@@ -1,7 +1,35 @@
 
 import { Graphics } from 'pixi.js';
 
-import { WHITE } from '../style/color';
+import { BLUE, BLUE1, BLUE2, BLUE3, BLUE4 } from '../style/color';
+
+
+/*
+ *get a random number between min and max
+ */
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/*
+ *return a random color
+ *TODO améliorer pour que ce soit pas du bricolage
+ */
+function getColor() {
+    const value = getRndInteger(1, 5);
+    if (value === 1) {
+        return BLUE4;
+    } else if (value === 2) {
+        return BLUE;
+    } else if (value === 3) {
+        return BLUE1;
+    } else if (value === 4) {
+        return BLUE2;
+    } else if (value === 5) {
+        return BLUE3;
+    }
+    return BLUE2;
+}
 
 class Circle extends Graphics {
     constructor({
@@ -10,7 +38,7 @@ class Circle extends Graphics {
         rotation = 0,
         alpha = 1,
         radius = 1,
-        color = WHITE,
+        color = getColor(),
         pivot = { x: 0, y: 0 },
     }) {
         super();
