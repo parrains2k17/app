f1066252467f0b4c4dd1f3f476e27ad17f15e15d
Guillaume Wuip <guillaume@wuips.com>
2017-03-12 18:48:09 +0100

    Add drag and zoom

--- public/js/components/Stage.js
+++ public/js/components/Stage.js
@@ -1,5 +1,10 @@
 
-import { Application } from 'pixi.js';
+import { Application, Container } from 'pixi.js';
+import { select, zoom, event } from 'd3';
+
+const SCALE_MIN_VALUE = 0.5;
+const SCALE_MAX_VALUE = 3;
+const DRAG_SPEED = 0.003;
 
 const devicePixelRatio = window.devicePixelRatio;
 
@@ -14,13 +19,43 @@ class Stage extends Application {
                 resolution: devicePixelRatio,
             },
         );
-        canvas.appendChild(this.view);
+
+        this.width = width;
+        this.height = height;
+        this.canvas = canvas;
+        this.canvas.appendChild(this.view);
+
+        this.createContainer();
 
         this.ticker.add(this.update.bind(this));
     }
 
+    createContainer() {
+        this.container = new Container();
+
+        const zoomHandler = zoom()
+            .scaleExtent([SCALE_MIN_VALUE, SCALE_MAX_VALUE])
+            .on('zoom', () => {
+                const
+                    scale = event.transform.k,
+                    x = event.transform.x,
+                    y = event.transform.y;
+
+                this.container.scale.set(scale, scale);
+
+                this.container.position.set(
+                    ((1 - scale) + (DRAG_SPEED * x)) * (this.width / 2),
+                    ((1 - scale) + (DRAG_SPEED * y)) * (this.height / 2)
+                );
+            });
+
+        select(this.canvas).call(zoomHandler);
+
+        this.stage.addChild(this.container);
+    }
+
     update() {
-        this.stage.children.forEach((child) => {
+        this.container.children.forEach((child) => {
             if (child.update) {
                 child.update();
             }
@@ -28,8 +63,9 @@ class Stage extends Application {
     }
 
     add(object) {
-        this.stage.addChild(object);
+        this.container.addChild(object);
     }
+
 }
 
 export default Stage;
