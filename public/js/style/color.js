dedaed46b2376d08d3b10e9de7320f43f61cd5d5
Guillaume Wuip <guillaume@wuips.com>
2017-03-12 16:24:54 +0100

    Refacto randomColor

--- public/js/style/color.js
+++ public/js/style/color.js
@@ -2,8 +2,4 @@
 export const PURPLE = 0x9966FF;
 export const BLACK  = 0x000000;
 export const WHITE  = 0xFFFFFF;
-export const BLUE  = 0x00FFFF;
-export const BLUE1  = 0xE6FFFF;
-export const BLUE2  = 0x99FFFF;
-export const BLUE3  = 0x00B3B3;
-export const BLUE4  = 0x006666;
+export const BLUE   = 0x00FFFF;
