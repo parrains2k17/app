92ce467282860b5694a964409df64e1340966fc8
Guillaume Wuip <guillaume@wuips.com>
2017-03-13 21:54:06 +0100

    Change style on candidate over

--- js/containers/Candidate.js
+++ js/containers/Candidate.js
@@ -1,7 +1,10 @@
 
-import { Container } from 'pixi.js';
+import { Container, Circle } from 'pixi.js';
 
+import { cursorClic, cursorDefault } from '../utils/cursor';
 import Supporters from './Supporters';
+
+const HIT_AREA_RADIUS = 100;
 
 class CirclesGroup extends Container {
     constructor(
@@ -19,9 +22,26 @@ class CirclesGroup extends Container {
         this.supporters = new Supporters(supporters);
         this.supporters.rotate(); // TODO maybe not here
 
+        this.interactive = true;
+        this.hitArea = new Circle(0, 0, HIT_AREA_RADIUS);
+
         this.addChild(this.candidate);
         this.addChild(this.supporters);
     }
+
+    mouseover() {
+        this.candidate.overState(true);
+        cursorClic();
+    }
+
+    mouseout() {
+        this.candidate.overState(false);
+        cursorDefault();
+    }
+
+    // mousedown() {
+    //     console.log('clic');
+    // }
 }
 
 export default CirclesGroup;
