b833559e59deb37e0e6a526c643fc48ef3d76c26
Guillaume Wuip <guillaume@wuips.com>
2017-03-13 21:54:06 +0100

    Change style on candidate over

--- public/js/components/Candidate.js
+++ public/js/components/Candidate.js
@@ -7,6 +7,14 @@ class Candidate extends MovingCircle {
     constructor(config = {}) {
         super({ radius: 10, color: PURPLE, ...config });
     }
+
+    overState(active = false) {
+        if (active) {
+            this.scale.set(1.2, 1.2);
+        } else {
+            this.scale.set(1, 1);
+        }
+    }
 }
 
 export default Candidate;
