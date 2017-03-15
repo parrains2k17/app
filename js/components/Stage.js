3a005620a02c7172ff290945145690f4100d345d
Guillaume Wuip <guillaume@wuips.com>
2017-03-15 05:37:08 +0100

    Hide other candidates and center screen on clic

--- js/components/Stage.js
+++ js/components/Stage.js
@@ -1,6 +1,9 @@
 
 import { Application, Container } from 'pixi.js';
 import { select, zoom, event } from 'd3';
+import { TweenMax, Power0 } from 'gsap';
+
+const RESET_DURATION = 0.3;
 
 const SCALE_MIN_VALUE = 0.5;
 const SCALE_MAX_VALUE = 3;
@@ -27,6 +30,8 @@ class Stage extends Application {
         this.createContainer();
 
         this.ticker.add(this.update.bind(this));
+
+        this.zoomable = true;
     }
 
     createContainer() {
@@ -35,6 +40,10 @@ class Stage extends Application {
         const zoomHandler = zoom()
             .scaleExtent([SCALE_MIN_VALUE, SCALE_MAX_VALUE])
             .on('zoom', () => {
+                if (!this.zoomable) {
+                    return;
+                }
+
                 const
                     scale = event.transform.k,
                     x = event.transform.x,
@@ -63,6 +72,30 @@ class Stage extends Application {
 
     add(object) {
         this.container.addChild(object);
+    }
+
+    center() {
+        this.zoomable = false;
+
+        TweenMax.to(
+            this.container.position,
+            RESET_DURATION,
+            {
+                x:    0,
+                y:    0,
+                ease: Power0.easeNone,
+            }
+        );
+
+        TweenMax.to(
+            this.container.scale,
+            RESET_DURATION,
+            {
+                x:    1,
+                y:    1,
+                ease: Power0.easeNone,
+            }
+        );
     }
 
 }
