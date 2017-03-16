dfdaa95fe741142932a6569dd55a7a9fb1187b08
Guillaume Wuip <guillaume@wuips.com>
2017-03-16 17:15:19 +0100

    Add todo

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ require('../sass/styles.scss');
 
 console.log(`🤖 Parrains2017 v${VERSION}`);// eslint-disable-line no-undef
 
+// TODO test webgl here and show modal if needed
+
 const app = new AppController();
 
 app.start();
