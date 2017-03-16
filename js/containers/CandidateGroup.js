11ecb6cc225affacbb1ee147f0195529b0faa793
Guillaume Wuip <guillaume@wuips.com>
2017-03-16 17:14:49 +0100

    Moving candidate groups

--- js/containers/CandidateGroup.js
+++ js/containers/CandidateGroup.js
@@ -5,10 +5,13 @@ import { TweenMax, Power0 } from 'gsap';
 import Supporters from './SupportersGroup';
 import Candidate from '../components/Candidate';
 
+const { random } = Math;
+
 const
     HIT_AREA_RADIUS         = 100,
     HIDE_DURATION           = 0.1,
-    MOVE_TO_CENTER_DURATION = 0.3;
+    MOVE_TO_CENTER_DURATION = 0.3,
+    MOVEMENT_DURATION       = 60;
 
 class CandidateGroup extends Container {
     constructor(
@@ -36,6 +39,8 @@ class CandidateGroup extends Container {
 
         this.screen = screen;
         this.controller = controller;
+
+        this.moveAround();
     }
 
     mousedown() {
@@ -91,6 +96,36 @@ class CandidateGroup extends Container {
         this.supporters.resetPosition();
         this.supporters.rotate();
         this.candidate.resetPosition({ MOVE_TO_CENTER_DURATION });
+    }
+
+    moveAround() { // TODO better waiting state
+        const { x, y } = this.position;
+
+        const
+            pos1 = { x: x + (100 * random()), y: y + (100 * random()) },
+            pos2 = { x: x - (100 * random()), y: y + (100 * random()) };
+
+        const [p1, p2] = (random() > 0.5) ? [pos1, pos2] : [pos2, pos1];
+
+        TweenMax.to(
+            this,
+            MOVEMENT_DURATION,
+            {
+                repeat: -1,
+                ease:   Power0.easeNone,
+                bezier: {
+                    type:      'thru',
+                    curviness: 10,
+                    values:    [
+                        { x, y },
+                        p1,
+                        { x, y: y + (200 * random()) },
+                        p2,
+                        { x, y },
+                    ],
+                },
+            }
+        );
     }
 }
 
