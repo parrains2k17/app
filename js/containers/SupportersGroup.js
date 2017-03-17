9426cb38b57a0c124cb1b35c9a4c3a7f42e13847
Thomas Coquereau <coquereau.thomas@gmail.com>
2017-03-17 10:29:52 +0100

    ADD random rotation direction

--- js/containers/SupportersGroup.js
+++ js/containers/SupportersGroup.js
@@ -46,9 +46,11 @@ class Supporters extends Container {
     }
 
     rotate() {
+        const direction = (random() > 0.5) ? 1 : -1;
         this.children.forEach((c) => c.rotate({
             // random to smooth transition
             duration: ROTATION_DURATION + (random() * 6),
+            direction,
         }));
     }
 
