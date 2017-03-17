113f1e6b3a134b13cb25bc0bcbae4188c9258d74
Jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2017-03-17 11:01:14 +0100

    fix crédits

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 import AppController from './AppController';
 import Modal from './components/Modal';
+import Credits from './components/Credits'
 
 require('../sass/styles.scss');
 
@@ -9,5 +10,6 @@ console.log(`🤖 Parrains2017 v${VERSION}`);// eslint-disable-line no-undef
 const app = new AppController();
 
 const modal = new Modal();
+const credits = new Credits();
 
 app.start();
