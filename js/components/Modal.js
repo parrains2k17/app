0ecede6c6601c995c7a6979777d7334b9ca51745
Jimmy <jimmy.leblanc@etu.univ-nantes.fr>
2017-03-19 18:47:11 +0100

    webGL message

--- js/components/Modal.js
+++ js/components/Modal.js
@@ -3,15 +3,21 @@ class Modal {
     constructor(type) {
         this.type = type;
 
+        this.text_container = document
+            .querySelector('.js-text-container');
+        this.modal_container = document
+            .querySelector('.js-modal_container_intro');
+        this.text_container = document
+            .querySelector('.js-text-container');
+        this.closeButton = this.modal_container
+            .querySelector('.js-close-modal');
+
         if (type === 'intro') {
+            this.creditsButton = document
+                .querySelector('.js-credits-link');
             this.modal_background = document
                 .querySelector('.js-modal_background');
-            this.modal_container = document
-                .querySelector('.js-modal_container_intro');
-            this.closeButton = this.modal_container
-                .querySelectorAll('.js-close-modal');
-
-            this.closeButton[0]
+            this.closeButton
                 .addEventListener('click', this.close.bind(this));
             // this.closeButton[1]
             //     .addEventListener('click', this.close.bind(this));
@@ -20,17 +26,26 @@ class Modal {
 
             this.open();
         } else if (type === 'webGL') {
-            this.modal_container = document
-                .querySelector('.js-modal_container_webGL');
+            this.text_container_webGL = document
+                .querySelector('.js-text-container-webGL');
             this.open();
         }
+
+        console.log(this.modal_container);
     }
 
     open() {
         if (this.type === 'intro') {
             this.modal_background.classList.add('mode-open');
+            this.modal_container.classList.add('mode-open');
+            this.closeButton.classList.add('mode-open');
+            this.creditsButton.classList.add('mode-open');
+        } else if (this.type === 'webGL') {
+            this.modal_container.classList.add('mode-open');
+            console.log(this.modal_container);
+            this.text_container.classList.remove('mode-open');
+            this.text_container_webGL.classList.add('mode-open');
         }
-        this.modal_container.classList.add('mode-open');
         this.modal_container.classList.add('mode-animate');
     }
 
@@ -39,5 +54,6 @@ class Modal {
         this.modal_container.classList.remove('mode-open');
     }
 }
+
 
 export default Modal;
