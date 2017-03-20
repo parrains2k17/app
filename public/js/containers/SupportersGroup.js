ade6527ca30779b2bddedf1a9ff421dd53c50eb6
Guillaume Wuip <guillaume@wuips.com>
2017-03-20 05:17:08 +0100

    Move each dataviz draw function into dataviz/

--- public/js/containers/SupportersGroup.js
+++ public/js/containers/SupportersGroup.js
@@ -1,9 +1,6 @@
 
 import { Container } from 'pixi.js';
-import { zip } from 'underscore';
-
 import { getWidth, getHeight } from '../utils/window';
-import { pointsPositionInRect } from '../utils/points';
 
 import Supporter from '../components/Supporter';
 // import randomColor from '../utils/randomColor';
@@ -22,11 +19,6 @@ import {
 } from '../dataviz';
 
 import {
-    barChart,
-    horizontalBarChart,
-} from '../dataviz/barchart';
-
-import {
     buildGenderData,
     buildCSPData,
     buildAgeData,
@@ -36,7 +28,13 @@ import {
     buildListData,
 } from '../dataviz/buildData';
 
-const { PI, random, sqrt, floor } = Math;
+import {
+    showBarChart,
+    showHorizontalBarChart,
+    showDotMatrix,
+} from '../dataviz/drawDataviz';
+
+const { PI, random, sqrt } = Math;
 
 const
     CENTER_DURATION   = 0.3,
@@ -49,97 +47,6 @@ const
 //     }
 //     return res;
 // };
-
-const showBarChart = (data, width, height, maxValue) => {
-    const bars = barChart({
-        data,
-        width,
-        height,
-        max: maxValue,
-    });
-
-    bars.forEach((bar) => {
-        const positions = pointsPositionInRect(
-            bar.value,
-            bar.width,
-            bar.height
-        );
-
-        zip(bar.points, positions)
-            .forEach(([point, position]) => {
-                point.position.x = (
-                    (-width / 2)
-                    + position.x
-                    + bar.x
-                );
-                point.position.y = (
-                    (height / 2)
-                    + (-position.y + bar.y)
-                );
-                point.alpha = 1;
-                point.changeColor(bar.color);
-            });
-    });
-
-    // TODO legend
-};
-
-const showHorizontalBarChart = (data, width, height, maxValue) => {
-    const bars = horizontalBarChart({
-        data,
-        width,
-        height,
-        max: maxValue,
-    });
-
-    bars.forEach((bar) => {
-        const positions = pointsPositionInRect(
-            bar.value,
-            bar.width,
-            bar.height
-        );
-
-        zip(bar.points, positions)
-            .forEach(([point, position]) => {
-                point.position.x = (
-                    (-width / 2)
-                    + position.x
-                    + bar.x
-                );
-                point.position.y = (
-                    -(height / 2)
-                    + (-position.y + bar.y)
-                );
-                point.alpha = 1;
-                point.changeColor(bar.color);
-            });
-    });
-
-    // TODO legend
-};
-
-const showDotMatrix = (points, colors, width) => {
-    const
-        w = 10,
-        h = 10;
-
-    const
-        r = floor(width / w), // number of points per line
-        maxHeight = (points.length / r) * h;
-
-    points.forEach((point, i) => {
-        const
-            x = (i % r) * w,
-            y = floor(i / r) * h;
-
-        point.position.x = (-width / 2) + x;
-        point.position.y = -(maxHeight / 2) + y;
-        point.alpha = 1;
-        point.changeColor(colors[i]);
-    });
-
-    // TODO legend
-};
 
 class Supporters extends Container {
     constructor(supporters) {
